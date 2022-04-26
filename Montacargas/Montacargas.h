755f41f131f166a53e066eb53d34e8c159b46cf4
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-04-26 00:46:15 -0300

    Create Montacargas.h

---
+++ Montacargas/Montacargas.h
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+class Montacargas {
+	// Private section
+	double pesoMaximo ;
+	double pesoActual ;
+	double TotalCargado;
+	int cantidadDeCargas;
+	
+	public:
+		// Public Declarations
+		Montacargas(double);
+		double obtenerCarga( ); //getCarga
+		double cargar(double); //setCarga.
+		double obtenerPesoRestanteLibre();
+		double descargar();
+		bool estaCargado();
+		double obtenerCargaPromedio();
+		double obtenerCantidadDeCargas();
+		double obtenerPesoTotal();
+		
+};
+
+Montacargas :: Montacargas(double _pesoMax){
+	
+	pesoMaximo = _pesoMax;
+	pesoActual = 0;
+	TotalCargado = 0;
+	cantidadDeCargas = 0;
+}
+
+double Montacargas :: obtenerCarga(){
+		return pesoActual;}
+
+double Montacargas :: cargar(double carga){
+	pesoActual +=carga;	
+	return carga;
+	}
+	
+double Montacargas :: obtenerPesoRestanteLibre(){
+	double pesoLibre = pesoMaximo - pesoActual;
+	return pesoLibre;
+}
+
+bool Montacargas :: estaCargado(){
+	return (this->obtenerCarga() > 0);
+}
+
+double Montacargas :: descargar(){
+	 
+	 if ((this->estaCargado()) && (obtenerCarga()<1000)){
+		TotalCargado += pesoActual;
+		cantidadDeCargas++;
+		pesoActual=0;
+			
+	 }
+	
+}
+double Montacargas :: obtenerCantidadDeCargas(){
+
+	return cantidadDeCargas;
+}
+
+double Montacargas :: obtenerPesoTotal(){
+	
+	return TotalCargado;
+}
+
+double Montacargas :: obtenerCargaPromedio(){
+	double cargaPromedio;
+	cargaPromedio = TotalCargado / cantidadDeCargas;
+	return cargaPromedio;
+
+}
