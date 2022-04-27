94455a4807c2b431c06ef59120dd97591b73ff9a
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-04-27 17:35:13 -0300

    Create README.cpp

---
+++ TDA Examen/README.cpp
@@ -0,0 +1,7 @@
+TDA Examen
+
+Diseñar la especificación e implementar el TDA Examen. Debe proveer operaciones para: 
+ • crear el Examen recibiendo como parámetro el nombre del alumno.
+ • calificar con una nota [1..10]. Solo debe ser posible calificar el Examen una única vez.
+ • obtener la nota.
+ • revisar la nota, es decir cambiarla pero solo por una superior.
