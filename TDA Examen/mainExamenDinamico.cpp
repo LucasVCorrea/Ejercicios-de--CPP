609389a05b7c2960ae5fcb63722aa7ebc1a805c7
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-05-03 22:46:40 -0300

    Update mainExamenDinamico.cpp

--- TDA Examen/mainExamenDinamico.cpp
+++ TDA Examen/mainExamenDinamico.cpp
@@ -8,17 +8,21 @@ int main() {
 	Exam *alumnoUno = new Exam("Correa");
 	
 	try{
+		
 		alumnoUno-> setNota(8.5);
 		cout<<"Al alumno "<<alumnoUno->getApellido()<<" se le puso un "<<alumnoUno->getNota()<<endl;
+		
 	}catch(char const*x){
 		
 		cout<<"ERROR: "<<x<<endl;
 	}
 	
 	try{
+		
 		alumnoUno-> setNotaModificada(2);
 		cout<<"Luego de revisar el examen se le cambio la nota por un "<<alumnoUno->getNotaModificada()<<endl;
 		cout<<"Entonces su nota final es: "<<alumnoUno->getNota();
+		
 	}catch(char const*y){
 	
 		cout<<"ERROR: "<<y<<endl;
