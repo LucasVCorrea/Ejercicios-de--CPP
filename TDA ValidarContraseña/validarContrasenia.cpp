a9b7e0835c175d68331bcaa3d83f2aeb145c6940
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-05-04 00:51:35 -0300

    Add files via upload

---
+++ TDA ValidarContraseña/validarContrasenia.cpp
@@ -0,0 +1,85 @@
+#include "validarContrasenia.h"
+#include <iostream>
+
+using namespace std;
+
+ValidarContrasenia::ValidarContrasenia(){
+	
+}
+
+ValidarContrasenia::~ValidarContrasenia(){
+	
+}
+
+	void ValidarContrasenia::setContrasenia(string cadena){
+		this->cadena = cadena;
+		
+	}
+
+	string ValidarContrasenia::getContrasenia(){
+		return this->cadena;
+	}
+
+
+	bool ValidarContrasenia::checkMayusc(){
+		int i = 0,contadorDeMayusculas=0;
+		
+		while (this->getContrasenia()[i]!='\0'){
+			
+			if ((this->getContrasenia()[i] >= 'A') && (this->getContrasenia()[i]) <= 'Z'){
+				contadorDeMayusculas++;
+					
+			}
+		i++;
+		}
+		
+		if (contadorDeMayusculas>0){
+			this->tieneMayuscula = true;
+		}
+		
+		else{
+			this->tieneMayuscula = false;
+			throw "La contraseña no tiene mayusculas.";
+		}		
+		return this->tieneMayuscula;
+	}
+
+	
+	
+	bool ValidarContrasenia::checkNumero(){
+		int i = 0, contadorDeNumeros = 0;
+		
+		while (this->getContrasenia()[i]!='\0'){
+			
+			if((this->getContrasenia()[i] >= '0') && (this->getContrasenia()[i] <= '9')){
+				contadorDeNumeros++;
+				
+			}
+		i++;	
+		}
+		
+		if (contadorDeNumeros > 0){
+			this->tieneNumero = true;
+		}
+		else{
+			this->tieneNumero = false;
+			throw "La contraseña no tiene ningun caracter numerico.";	
+		}
+		return this->tieneNumero;
+		
+	}
+	
+	
+	bool  ValidarContrasenia::verSiEsValida(){
+		if ((this->checkMayusc()) && (this->checkNumero())){
+			this->esValida = true;
+			cout<<"Su contraseña es valida."<<endl;
+		}
+		
+		else{
+
+			throw "Su contraseña no es valida.";
+		}
+	
+	return this->esValida;
+	}
---
+++ TDA ValidarContraseña/validarContrasenia.h
@@ -0,0 +1,43 @@
+#ifndef VALIDARCONTRASENIA_H
+#define VALIDARCONTRASENIA_H
+#include <string>
+
+class ValidarContrasenia
+{
+	private:
+		std::string cadena;
+		bool esValida;
+		bool tieneMayuscula;
+		bool tieneNumero;
+	
+	public:
+		ValidarContrasenia();
+		~ValidarContrasenia();
+		
+		
+		/*
+			Pre: La contraseña debe tener almenos un numero y una mayuscula.
+			Post: Se acepta la contraseña.
+		*/
+		void setContrasenia(std::string);
+
+		std::string getContrasenia();
+		
+		/*
+			devuelve true si la contraseña tiene almenos un caracter numerico.
+		*/
+		bool checkNumero();
+		
+		/*
+			devuelve true si la contraseña tiene almenos una mayusucla.
+		*/
+		bool checkMayusc();
+		
+		/*
+			devuelve true si la contraseña tiene un numero y una mayuscula.
+		*/
+		bool verSiEsValida();
+	
+};
+
+#endif
