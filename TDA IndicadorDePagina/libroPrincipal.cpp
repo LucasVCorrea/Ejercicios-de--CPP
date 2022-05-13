8823423ffa17da71cf88e1bd988d9b974b0ba828
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-05-13 11:08:03 -0300

    Update libroPrincipal.cpp

--- TDA IndicadorDePagina/libroPrincipal.cpp
+++ TDA IndicadorDePagina/libroPrincipal.cpp
@@ -5,20 +5,19 @@ using namespace std;
 
 int main(){
 	
-	IndicadorDePagina *miLibro = new IndicadorDePagina("MoonKnight",150);
-	cout<<"Paginas totales de "<<miLibro->getNombreLibro()<<" : "<<miLibro->getPaginasTotales()<<endl<<endl;
-
-
-	miLibro->pasarDePagina();
-	miLibro->pasarDePagina();
-	miLibro->pasarDePagina();
-	miLibro->volverUnaPagina();
-	miLibro->pasarDePagina();
-	miLibro->pasarDePagina();
+	IndicadorDePagina *miLibro = new IndicadorDePagina("MoonKnight",15);
+	cout<<"Paginas totales de "<<miLibro->getNombreLibro()<<": "<<miLibro->getPaginasTotales()<<endl<<endl;
+	try{
 	
-	
+	miLibro->pasarDePagina();
+
 	cout<<"El libro esta en la pagina "<<miLibro->getPaginaActual()<<endl<<endl;
-	cout<<"Quedan "<<miLibro->getPaginasPorLeerRestantes()<<" paginas por leer"<<endl;
+	cout<<"Quedan "<<miLibro->getPaginasPorLeerRestantes()<<" paginas hacia el final"<<endl;
+	
+	}catch(const char*a){
+		cout<<a<<endl;
+	}
+
 	delete miLibro;
 	return 0;
 }
