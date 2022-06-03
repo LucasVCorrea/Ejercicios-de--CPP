15353c7cce1cfe34f944c74ec6d2bd0fcad65f3e
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-06-03 19:11:23 -0300

    Update Restaurante.h

--- TDA´s/TDA Restaurante/Restaurante.h
+++ TDA´s/TDA Restaurante/Restaurante.h
@@ -2,14 +2,7 @@
 #define RESTAURANTE_H
 #include "Mesa.h"
 #include "Vector.h"
-/*
-4. Diseñar la especificación e implementar el TDA Restaurante. Debe proveer operaciones para:
-• Construir el Restaurante a partir de la cantidad de mesas que tiene. Cada mesa se identifica con un número.
-• adicionarEnMesa: agrega el monto dado [$] a la mesa indicada, dejándola abierta.
-• cerrarMesa: devuelve el total de la mesa indicada y la deja libre.
-• contarMesasLibres: cuenta la cantidad de mesas que están libres.
-• totalizarRecaudacion: devuelve el total recaudado por el Restaurante
-*/
+
 class Restaurante
 {
 	private:
