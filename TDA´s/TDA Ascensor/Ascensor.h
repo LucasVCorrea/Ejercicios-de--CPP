b3425430ff72f3b0aa4490605f0a24502b3d40b0
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-06-07 11:40:27 -0300

    Update Ascensor.h

--- TDA´s/TDA Ascensor/Ascensor.h
+++ TDA´s/TDA Ascensor/Ascensor.h
@@ -1,34 +1,72 @@
 #ifndef ASCENSOR_H
 #define ASCENSOR_H
+#include "Vector.h"
 #include "Piso.h"
+
 class Ascensor
 {
 	private:
-		int cantidadDePisos,totalDeSubidas,totalDeBajadas,pisoActual;
-		int desplazamiento;
-		Piso** pisos;
-		void validarDireccionamiento(int piso);
-	public:
-		Ascensor(int cantidadDePisos);
+		unsigned int cantidadPisos;
+		unsigned int pisoActual;
+		unsigned int subidas;
+		unsigned int bajadas;
 		
+		Vector<Piso *> *pisos;
+		
+	public:
+		
+		/*
+			pre: la cantidad de pisos debe ser mayor o igual a 1
+			pos: se instancia el ascensor con la cantidad de pisos indicada
+		*/
+		Ascensor(unsigned int cantidadPisos);
+		
+		/*
+			pre:
+			pos: se libera memoria
+		*/
 		virtual ~Ascensor();
 		
-		void llamarDesde(int pisoDeLlamada);
+		/*
+			pre: - 
+			pos: devuelve la cantidad de pisos que recorre el ascensor
+		*/
+		unsigned int getCantidadPisos();
 		
-		int getCantidadDePisos();
+		/*
+			pre: - 
+			pos: devuelve el piso en el que se encuentra el ascensor
+		*/
+		unsigned int getPisoActual();
 		
-		int getCantidadDeVecesQueFueAlPiso(int piso);
+		/*
+			pre: el piso debe estar en el recorrido
+			pos: se mueve el ascensor hacia ese piso y devuelve la cantidad de pisos desplazados
+		*/
+		unsigned int llamarAscensor(unsigned int piso);
 		
-		int getPisoActual();
+		/*
+			pre: el piso debe estar en el recorrido
+			pos: devuelve la cantidad de veces que se fue a ese piso
+		*/
+		unsigned int verHistorico(unsigned int piso);
 		
-		int getTotalDePisosSubidos();
+		/*
+			pre: -
+			pos: devuelve la cantidad de veces que subio
+		*/
+		unsigned int pisosSubidos();
 		
-		int getTotalDePisosBajados();
-		
-		int getDesplazamiento();
-		
-		int getPiso(int pisoDeMuestra);
-		
+		/*
+			pre: -
+			pos: devuelve la cantidad de veces que bajo
+		*/
+		unsigned int pisosBajados();
+	
+	private:
+		bool esValido(unsigned int piso);
+		void validarPiso(unsigned int piso);
+		unsigned int recorrido(unsigned int piso);
 };
 
 #endif
