93474d8bfc3ad3fdaaed8689ecdc2e281b514a0c
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-06-27 12:22:51 -0300

    Create main.cpp

---
+++ Herramientas/notasDeAlumnos/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	
+	int cantidadAlumnos;
+	string fecha;
+	ofstream archivo("NotasAlumnos.txt", std::fstream::app);
+	cout<<"Ingrese fecha del examen (DD/MM/AA): ";cin>>fecha;
+	archivo<<"|Fecha: "<<fecha<<" |"<<endl;
+	cout<<"Ingrese la cantidad de alumnos: ";cin>>cantidadAlumnos;
+	
+	string arrClave[cantidadAlumnos];
+	double arrNotas[cantidadAlumnos];
+	
+	for(int i = 0; i < cantidadAlumnos; i++){	
+		
+		if (i == cantidadAlumnos - 1){
+			cout<<"ultimo alumno "<<endl;
+		}
+		
+		cout<<"Ingrese el apellido del alumno "<< i + 1 <<": ";cin>>arrClave[i];
+		cout<<"Ingrese la nota de "<<arrClave[i]<<": ";cin>>arrNotas[i];
+		archivo<<arrClave[i]<<" - "<<arrNotas[i]<<endl;
+	}
+	archivo<<endl;
+	
+	archivo.close();
+	return 0;
+}
