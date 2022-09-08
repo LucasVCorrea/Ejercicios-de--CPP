dcee243bf1350d4eef918c354f0d641f2c3af5a9
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-09-08 13:33:33 -0300

    Create ahorcado.cpp

---
+++ Ahorcado/ahorcado.cpp
@@ -0,0 +1,61 @@
+#include "Ahorcado.h"
+
+Ahorcado::Ahorcado(){
+	this->contador = 5;
+	this->intentos = 0;
+	this->listaPalabras = new std::string[10] {"cars","avengers","harry-potter","shrek","she-hulk","thor","daredevil","venom","ant-man","wolverine"};
+	this->suma = 0;
+	this->valor = rand() %10 + 1;
+	this->palabra = this->listaPalabras[this->valor];
+	this->palabraAux = this->palabra;
+}
+
+void Ahorcado::parteUno(){
+	for(int i = 0; i < this->palabra.size(); i++){
+		this->palabraAux[i] = '_';	
+	}
+}
+
+void Ahorcado::ciclar(){
+	int j = 0;
+	while (this->contador!=0 && this->palabra != palabraAux){ 
+		
+		
+		this->intentos++; 
+		cout<<"Ingrese una letra: ";cin>>this->letra;cout<<endl;
+		for(int i = 0; i < this->palabra.size(); i++){
+			
+			if (this->palabra[i] == this->letra){
+				this->palabraAux[i] = this->letra;
+				this->suma++;
+			}
+			cout<<this->palabraAux[i]<<' ';
+		}
+		
+		cout<<endl;
+		
+		if(this->suma == 0){
+			this->contador--;
+		}
+		
+		cout<<endl<<"vidas: "<<this->contador<<endl<<endl;
+		this->suma = 0;
+		j++;
+	}
+}
+
+void Ahorcado::final(){
+		if(this->contador == 0){
+		cout<<"Mas suerte la proxima!";
+	}
+	else{
+		cout<<"Felicitaciones! has adivinado la palabra en "<<this->intentos<<" intentos";
+	}
+}
+
+void Ahorcado::Jugar(){
+	cout<<"(tener en cuenta: las peliculas con mas de una palabra se separan con un guion '-' )"<<endl<<endl;
+	this->parteUno();
+	this->ciclar();
+	this->final();
+}
