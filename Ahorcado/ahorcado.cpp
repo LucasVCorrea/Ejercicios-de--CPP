6276ac215d2cb7eeee87611a97c7c1c8361e09ea
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-09-10 15:32:15 -0300

    Update ahorcado.cpp

--- Ahorcado/ahorcado.cpp
+++ Ahorcado/ahorcado.cpp
@@ -1,12 +1,12 @@
 #include "Ahorcado.h"
 
 Ahorcado::Ahorcado(){
-	this->contador = 6;
+	this->vidas = 6;
 	this->intentos = 0;
-	this->listaPalabras = new std::string[10] {"cars","avengers","harry-potter","shrek","she-hulk","thor","daredevil","venom","ant-man","wolverine"};
-	this->suma = 0;
-	this->valor = rand() %10 + 1;
-	this->palabra = this->listaPalabras[this->valor];
+	this->listaPalabras = new std::string[12] {"morbius","iron-man","spider-man","capitan-america","she-hulk","thor","daredevil","venom","ant-man","x-men","moonknight","luke-cage"};
+	this->letrasEncontradas = 0;
+	this->randomNumber = this->generaRandomNumber() ;
+	this->palabra = this->listaPalabras[this->randomNumber];
 	this->palabraAux = this->palabra;
 }
 
@@ -20,20 +20,20 @@ void Ahorcado::parteUno(){
 
 void Ahorcado::ciclar(){
 	int j = 0;
-	while (this->contador!=0 && this->palabra != palabraAux){ 
+	while (this->vidas!=0 && this->palabra != palabraAux){ 
 		
 		
-		if(this->contador == 6){
+		if(this->vidas == 6){
 			cout<<endl<<" ___"<<endl<<"|   |"<<endl<<"|"<<endl<<"|"<<endl<<"|"<<endl;
-		}else if(this->contador == 5){
+		}else if(this->vidas == 5){
 			cout<<endl<<" ___"<<endl<<"|   |"<<endl<<"|   O"<<endl<<"|"<<endl<<"|"<<endl;
-		}else if(this->contador == 4){
+		}else if(this->vidas == 4){
 			cout<<endl<<" ___"<<endl<<"|   |"<<endl<<"|   O"<<endl<<"|   |"<<endl<<"|"<<endl;
-		}else if(this->contador == 3){
+		}else if(this->vidas == 3){
 			cout<<endl<<" ___"<<endl<<"|   |"<<endl<<"|   O"<<endl<<"|  /| "<<endl<<"|"<<endl;
-		}else if(this->contador == 2){
+		}else if(this->vidas == 2){
 			cout<<endl<<" ___"<<endl<<"|   |"<<endl<<"|   O"<<endl<<"|  /|\\ "<<endl<<"|"<<endl;
-		}else if(this->contador == 1){
+		}else if(this->vidas == 1){
 			cout<<endl<<" ___"<<endl<<"|   |"<<endl<<"|   O"<<endl<<"|  /|\\ "<<endl<<"|  /"<<endl;
 		}
 
@@ -45,36 +45,41 @@ void Ahorcado::ciclar(){
 			
 			if (this->palabra[i] == this->letra){
 				this->palabraAux[i] = this->letra;
-				this->suma++;
+				this->letrasEncontradas++;
 			}
 			cout<<this->palabraAux[i]<<' ';
 		}
 		
 		cout<<endl;
 		
-		if(this->suma == 0){
-			this->contador--;
+		if(this->letrasEncontradas == 0){
+			this->vidas--;
 		}
 		
 
 
 
-		cout<<endl<<"vidas: "<<this->contador<<endl<<endl;
-		this->suma = 0;
+		cout<<endl<<"vidas: "<<this->vidas<<endl<<endl;
+		this->letrasEncontradas = 0;
 		j++;
 	}
-	if(this->contador == 0){
+	if(this->vidas == 0){
 			cout<<endl<<" ___"<<endl<<"|   |"<<endl<<"|   O"<<endl<<"|  /|\\ "<<endl<<"|  / \\"<<endl;
 	}
 }
 
 void Ahorcado::final(){
-		if(this->contador == 0){
+		if(this->vidas == 0){
 		cout<<endl<<"Mas suerte la proxima!";
 	}
 	else{
 		cout<<endl<<"Felicitaciones! has adivinado la palabra en "<<this->intentos<<" intentos";
 	}
+	cout<<endl<<"Quieres jugar otra vez? (s/n): ";cin>>this->opcion;
+	if(this->opcion == 's'){
+		this->Jugar();
+	}
+	
 }
 
 void Ahorcado::Jugar(){
@@ -82,4 +87,18 @@ void Ahorcado::Jugar(){
 	this->parteUno();
 	this->ciclar();
 	this->final();
+}
+
+Ahorcado::~Ahorcado(){
+	
+}
+
+int Ahorcado::generaRandomNumber(){
+	srand(time(0)); // Initialize random number generator.
+
+	for(int i=0;i<1;i++){
+		this->randomNumber = (rand() % 10);
+	}
+
+    return this->randomNumber;
 }
